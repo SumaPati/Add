d728cf9aab8ed5c1492a27fecf2e607610dcf7d1
SumaPati <129846944+SumaPati@users.noreply.github.com>
2023-04-11 17:31:32 +0530

    textfoematting

---
+++ Add/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Add
+{
+
+    public class Mathop
+    {
+        public static int Sum(int num1, int num2)
+        {
+            int total;
+            total = num1 + num2;
+            return total;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            int total;
+            total = num1 - num2;
+            return total;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            int total;
+            total = num1 * num2;
+            return total;
+        }
+        public static int Divide(int num1, int num2)
+        {
+            int total;
+            total = num1 / num2;
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter two numbers: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nThe diff of two numbers is : {0} \n", Subtract(number1, number2));
+
+            Console.ReadLine();
+        }
+    }
+}
